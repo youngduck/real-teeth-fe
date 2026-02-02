2053f34e9f2078d0527ee691915f994dac5a0e61
youngduck <dudejr5839@naver.com>
2026-02-02 16:30:55 +0900

    feat: 날씨 상세 페이지 및 라우팅 추가

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { NotFoundPage } from "@pages/not-found/not-found-page";
 import HomePage from "@pages/home/home-page";
+import WeatherDetailPage from "@pages/weather-detail/weather-detail-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/weather/:address",
+    element: <WeatherDetailPage />,
   },
   {
     path: "*",
